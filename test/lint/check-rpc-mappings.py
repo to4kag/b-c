faec22c4a1019371a985b91547dab7d8f74e0d0e
MarcoFalke <falke.marco@gmail.com>
2019-01-31 14:06:14 -0500

    scripted-diff: Fix python DeprecationWarning: invalid escape sequence
    -BEGIN VERIFY SCRIPT-
    sed -i -e "s/re.search('/re.search(r'/g" $(git grep -l 're.search(')
    sed -i -e 's/re.search("/re.search(r"/g' $(git grep -l 're.search(')
    -END VERIFY SCRIPT-

--- test/lint/check-rpc-mappings.py
+++ test/lint/check-rpc-mappings.py
@@ -54,7 +54,7 @@ def process_commands(fname):
                 if line.startswith('};'):
                     in_rpcs = False
                 elif '{' in line and '"' in line:
-                    m = re.search('{ *("[^"]*"), *("[^"]*"), *&([^,]*), *{([^}]*)} *},', line)
+                    m = re.search(r'{ *("[^"]*"), *("[^"]*"), *&([^,]*), *{([^}]*)} *},', line)
                     assert m, 'No match to table expression: %s' % line
                     name = parse_string(m.group(2))
                     args_str = m.group(4).strip()
@@ -80,7 +80,7 @@ def process_mapping(fname):
                 if line.startswith('};'):
                     in_rpcs = False
                 elif '{' in line and '"' in line:
-                    m = re.search('{ *("[^"]*"), *([0-9]+) *, *("[^"]*") *},', line)
+                    m = re.search(r'{ *("[^"]*"), *([0-9]+) *, *("[^"]*") *},', line)
                     assert m, 'No match to table expression: %s' % line
                     name = parse_string(m.group(1))
                     idx = int(m.group(2))
