890b4aea030031a4f7f0833ae8fdbe409398c94d
MarcoFalke <falke.marco@gmail.com>
2020-08-28 13:17:27 +0200

    Revert "ci: Install llvm to get llvm symbolizer"
    This reverts commit fa7c8509153bfd2d5b4dcff86ad27dfd73e8788b.

--- ci/test/00_setup_env_native_tsan.sh
+++ ci/test/00_setup_env_native_tsan.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_tsan
 export DOCKER_NAME_TAG=ubuntu:20.04
-export PACKAGES="clang llvm libc++abi-dev libc++-dev python3-zmq"
+export PACKAGES="clang libc++abi-dev libc++-dev python3-zmq"
 export DEP_OPTS="CC=clang CXX='clang++ -stdlib=libc++'"
 export TEST_RUNNER_EXTRA="--exclude feature_block"  # Low memory on Travis machines, exclude feature_block.
 export GOAL="install"
